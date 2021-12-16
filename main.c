8df01252965537a3237810dfebdf07a5938f4f3f
Thiago Goveia <t.r.goveia@gmail.com>
2021-12-16 17:17:17 -0300

    Adds elitism and mutation

--- main.c
+++ main.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include "graph.h"
 
-typedef struct Node
-{
-    int distance;
-    int *path;
-} Node;
 #define NUM_NODES 20
-#define VECTOR_MAX_SIZE 25
 #define NUM_CHILDREN 100
 #define RANDOM_SEED 42
+#define MUTATION_RATE 2
 
-Node graph[VECTOR_MAX_SIZE * VECTOR_MAX_SIZE];
-int parentA[VECTOR_MAX_SIZE];
-int parentB[VECTOR_MAX_SIZE];
-int population[NUM_CHILDREN][VECTOR_MAX_SIZE];
-int occupied[VECTOR_MAX_SIZE];
-int bestOfAll[VECTOR_MAX_SIZE]; //fodelona
+int child[NUM_NODES];
+int population[NUM_CHILDREN][NUM_NODES];
+int occupied[NUM_NODES];
+int bestOfAll[NUM_NODES]; //fodelona
+int bestScore = INT_MAX;
 int bestofAllIndex;
 int seedPath[NUM_NODES] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
 
+void elitism();
 void evaluate();
 void emptyOccupied();
 void crossover(int *parentA, int *parentB, int *child);
@@ -29,35 +25,85 @@ void createRandomPath(int *path);
 void populate();
 void shuffle(int *array);
 void copyArray(int *source, int *destination);
+void printArray(int *array);
+int getDistance(int A, int B);
+int calculateTotalDistance(int *array);
+void printPopulation();
 
 int main()
 {
-    srand((unsigned)RANDOM_SEED);
     populate();
-    // print populations
+    while (1) {
+        evaluate();
+        elitism();
+        // printPopulation();
+        printf("%d\n", bestScore);
+    }
+}
+
+void printPopulation()
+{
+    for (int i = 0; i < NUM_CHILDREN; i++) {
+        printArray(population[i]);
+    }
+}
+
+void elitism()
+{
+    int bestScoreIndex = 0;
     for (int i = 0; i < NUM_CHILDREN; i++)
     {
-        for (int j = 0; j < NUM_NODES; j++)
-        {
-            printf("%d ", population[i][j]);
+        if (i != bestofAllIndex) {
+            crossover(population[i], population[bestofAllIndex], child);
+            mutation();
+            copyArray(child, population[i]);
         }
-        printf("\n");
     }
-    // shuffle(seedPath);
-    // for (int i = 0; i < NUM_NODES; i++)
-    // {
-    //     printf("%d ", seedPath[i]);
-    // }
-    // printf("\n");
+}
+
+int calculateTotalDistance(int *array)
+{
+    int totalDistance = 0;
+    for (int i = 0; i < NUM_NODES - 1; i++)
+    {
+        totalDistance += getDistance(array[i], array[i + 1]);
+    }
+    return totalDistance;
+}
+
+int getDistance(int A, int B)
+{
+    return completeGraph[A][B];
+}
+
+void printArray(int *array)
+{
+    for (int i = 0; i < NUM_NODES; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
 }
 
 void evaluate()
 {
+    int totalDistance;
+    for (int i = 0; i < NUM_CHILDREN; i++)
+    {
+        totalDistance = calculateTotalDistance(population[i]);
+        // printf("----%d\n", totalDistance);
+        if (totalDistance < bestScore)
+        {
+            bestScore = totalDistance;
+            copyArray(population[i], bestOfAll);
+            bestofAllIndex = i;
+        }
+    }
 }
 
 void emptyOccupied()
 {
-    for (int i = 0; i < VECTOR_MAX_SIZE; i++)
+    for (int i = 0; i < NUM_NODES; i++)
     {
         occupied[i] = 0;
     }
@@ -67,15 +113,15 @@ void crossover(int *parentA, int *parentB, int *child)
 {
     emptyOccupied();
     int counter = 0;
-    for (int i = 0; i < VECTOR_MAX_SIZE; i++)
+    for (int i = 0; i < NUM_NODES; i++)
     {
-        if (counter < VECTOR_MAX_SIZE && occupied[parentA[i]] == 0)
+        if (counter < NUM_NODES && occupied[parentA[i]] == 0)
         {
             child[counter] = parentA[i];
             occupied[parentA[i]] = 1;
             counter++;
         }
-        if (counter < VECTOR_MAX_SIZE && occupied[parentB[i]] == 0)
+        if (counter < NUM_NODES && occupied[parentB[i]] == 0)
         {
             child[counter] = parentB[i];
             occupied[parentB[i]] = 1;
@@ -84,7 +130,15 @@ void crossover(int *parentA, int *parentB, int *child)
     }
 }
 
-void mutation() {}
+void mutation() {
+    for (int i = 0; i < MUTATION_RATE; i++) {
+        int randomIndex1 = rand() % NUM_NODES;
+        int randomIndex2 = rand() % NUM_NODES;
+        int temp = child[randomIndex1];
+        child[randomIndex1] = child[randomIndex2];
+        child[randomIndex2] = temp;
+    }
+}
 
 void createRandomPath(int *path)
 {
